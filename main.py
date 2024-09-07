940b49282092fb4583aaa413bd828ba2ce5dd9c5
Aecio Batista <aecio.batista@hotmail.com>
2024-09-07 20:56:47 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Jornada de Dados")
+
+# Variáveis
+idade = 44
+nome = "Aecio"
+
+# Print
+print("idade")
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+
+# Metados
+lista_invertida = lista.reverse
+print(lista_invertida)
